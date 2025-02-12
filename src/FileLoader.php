09c1821a2af53e8393a5624371ee44e427cbfd4c
TheForestSystem <mfcpij@gmail.com>
2025-02-12 15:41:54 -0600

    Comments

--- src/FileLoader.php
+++ src/FileLoader.php
@@ -2,8 +2,24 @@
 
 namespace Foxxything\MarkdownFiller;
 
+/**
+ * Class to load a file's content from the specified file path.
+ */
 class FileLoader implements FileLoaderInterface
 {
+    /**
+     * Load the content of a file.
+     *
+     * This method reads the contents of the file at the given file path. If the file does
+     * not exist or cannot be read, an exception will be thrown.
+     *
+     * @param string $filePath The path to the file to be loaded.
+     * 
+     * @return string The content of the file.
+     * 
+     * @throws \InvalidArgumentException If the file does not exist.
+     * @throws \RuntimeException If the file cannot be read.
+     */
     public function load(string $filePath): string
     {
         if (!file_exists($filePath)) {
@@ -18,4 +34,3 @@ class FileLoader implements FileLoaderInterface
         return $content;
     }
 }
-
--- src/MarkdownFiller.php
+++ src/MarkdownFiller.php
@@ -1,18 +1,43 @@
 <?php
+
 namespace Foxxything\MarkdownFiller;
 
+/**
+ * Class to load a Markdown file and replace variables within the content.
+ */
 class MarkdownFiller implements MarkdownFillerInterface
 {
+    /**
+     * The content of the file after variable replacement.
+     * 
+     * @var string
+     */
     private string $fileContent;
 
+    /**
+     * Constructor to initialize file path and file loader.
+     * 
+     * @param string $filePath The path to the Markdown file.
+     * @param FileLoaderInterface|null $fileLoader The file loader used to load the content. If not provided, a default FileLoader will be used.
+     */
     public function __construct(
         private string $filePath,
-        private FileLoaderInterface $fileLoader = new FileLoader()
+        private ?FileLoaderInterface $fileLoader = null
     ) {
-        $this->fileLoader = $fileLoader;
+        $this->fileLoader = $fileLoader ?? new FileLoader();
         $this->fileContent = $this->fileLoader->load($this->filePath);
     }
 
+    /**
+     * Replace placeholders in the file content with values from the provided array.
+     *
+     * The method will search for placeholders in the form of [variableName] and replace
+     * them with their corresponding values from the $variables array.
+     *
+     * @param array $variables An associative array where the key is the variable name and the value is the replacement value.
+     * 
+     * @return string The modified content with replaced variables.
+     */
     public function replaceVars(array $variables): string
     {
         foreach ($variables as $varName => $varValue) {
@@ -22,6 +47,11 @@ class MarkdownFiller implements MarkdownFillerInterface
         return $this->fileContent;
     }
 
+    /**
+     * Get the current content of the file after variable replacement.
+     *
+     * @return string The content of the file.
+     */
     public function getContent(): string
     {
         return $this->fileContent;
